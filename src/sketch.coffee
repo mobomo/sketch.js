db2ad3b2a8df3f3b28785e9de4ffa500ad93b482
Michael Bleigh <michael@intridea.com>
2011-08-19 20:48:03 -0300

    Round one of object oriented refactor, tools are now modularly definable.

--- src/sketch.coffee
+++ src/sketch.coffee
@@ -1,85 +1,107 @@
 (($)->
+  $.sketch = { tools: {} }
+
+  class Sketch
+    constructor: (el, opts)->
+      @el = el
+      @canvas = $(el)
+      @context = el.getContext '2d'
+      @options = $.extend {
+        toolLinks: true
+        defaultTool: 'marker'
+        defaultColor: '#000000'
+        defaultSize: 5
+      }, opts
+
+      @painting = false
+      @color = @options.defaultColor
+      @size = @options.defaultSize
+      @tool = @options.defaultTool
+      @actions = []
+      @action = []
+
+    startPainting: ->
+      @painting = true
+      @action = {
+        tool: @tool
+        color: @color
+        size: @size
+        events: []
+      }
+
+    stopPainting: ->
+      @actions.push @action
+      @painting = false
+      @action = null
+      @redraw()
+    
+    onEvent: (e)->
+      $(this).data('sketch').addEvent e
+
+    addEvent: (e)->
+      switch e.type
+        when 'mousedown'
+          @startPainting()
+        when 'mouseup', 'mouseout'
+          @stopPainting()
+      
+      if @painting
+        @action.events.push
+          x: e.pageX - @canvas.offset().left
+          y: e.pageY - @canvas.offset().top
+          event: e.type
+          color: @color
+          size: @size
+          tool: @tool
+        @redraw()
+
+    redraw: ->
+      @el.width = @el.width
+      sketch = this
+      $.each @actions, ->
+        $.sketch.tools[this.tool].draw.call sketch, this
+      $.sketch.tools[@action.tool].draw.call sketch, @action if @painting
+
   $.fn.sketch = (opts)->
     $.error('Sketch can only be called on one element at a time.') if this.length > 1
+    
+    this.data('sketch', new Sketch(this.get(0), opts))
 
-    options = $.extend {
-      toolLinks: true
-    }, opts
-    el = this.get(0)
-    $el = $(el)
-    context = el.getContext '2d'
-
-    $el.data 'sketch.clicks', []
-    $el.data 'sketch.painting', false
-    $el.data 'sketch.color', '#000000'
-
-    painting = ->
-      !!$el.data('sketch.painting')
-    startPainting = ->
-      $el.data 'sketch.painting', true
-    stopPainting = ->
-      $el.data 'sketch.painting', false
-    clicks = ->
-      $el.data 'sketch.clicks'
-    currentColor = ->
-      $el.data 'sketch.color'
-
-    addClick = (x,y,dragging)->
-      clicks().push {x: x, y: y, dragging: dragging, color: currentColor()}
-
-    redraw = ()->
-      el.width = el.width
-      
-      context.lineJoin = 'round'
-      context.lineWidth = 5
-
-      previous = null
-      $.each clicks(), ->
-        context.beginPath()
-        if this.dragging && previous
-          context.moveTo previous.x, previous.y
-        else
-          context.moveTo this.x - 1, this.y
-
-        context.lineTo this.x, this.y
-        context.closePath()
-        context.strokeStyle = this.color
-        context.stroke()
-
-        previous = this
-
-    $el.bind 'sketch.changecolor', (e, color)->
-      console.log "Setting color to #{color}"
+    this.bind 'sketch.changecolor', (e, color)->
       $el.data 'sketch.color', color
 
-    $el.mousedown (e)->
-      mouseX = e.pageX - this.offsetLeft
-      mouseY = e.pageY - this.offsetTop
+    this.mousedown this.data('sketch').onEvent
+    this.mousemove this.data('sketch').onEvent
+    this.mouseup this.data('sketch').onEvent
+    this.mouseleave this.data('sketch').onEvent
 
-      startPainting()
-      addClick mouseX, mouseY
-      redraw()
-
-    $el.mousemove (e)->
-      if painting()
-        mouseX = e.pageX - this.offsetLeft
-        mouseY = e.pageY - this.offsetTop
-
-        addClick mouseX, mouseY, true
-        redraw()
-
-    $el.mouseup stopPainting
-    $el.mouseleave stopPainting
-
-    if options.toolLinks
-      console.log "Tool links..."
-      $('body').delegate "a[href=\"##{$el.attr('id')}\"]", 'click', (e)->
-        console.log "Tool clicked..."
+    that = this
+    if this.data('sketch').options.toolLinks
+      $('body').delegate "a[href=\"##{that.attr('id')}\"]", 'click', (e)->
         $this = $(this)
-
         if $this.attr('data-color')
-          $el.trigger 'sketch.changecolor', $(this).attr('data-color')
-
+          that.trigger 'sketch.changecolor', $(this).attr('data-color')
         false
+
     this
+
+  $.sketch.tools.marker =
+    draw: (action)->
+      @context.lineJoin = "round"
+      
+      previous = null
+      for event in action.events
+        @context.beginPath()
+        if previous
+          @context.moveTo previous.x, previous.y
+        else
+          @context.moveTo event.x - 1, event.y
+
+        @context.lineTo event.x, event.y
+        @context.closePath()
+        @context.strokeStyle = action.color
+        @context.lineWidth = action.size
+        @context.stroke()
+
+        previous = event
 )(jQuery)
--- src/sketch.js
+++ src/sketch.js
@@ -1,98 +1,129 @@
 (function() {
   (function($) {
-    return $.fn.sketch = function(opts) {
-      var $el, addClick, clicks, context, currentColor, el, options, painting, redraw, startPainting, stopPainting;
+    var Sketch;
+    $.sketch = {
+      tools: {}
+    };
+    Sketch = (function() {
+      function Sketch(el, opts) {
+        this.el = el;
+        this.canvas = $(el);
+        this.context = el.getContext('2d');
+        this.options = $.extend({
+          toolLinks: true,
+          defaultTool: 'marker',
+          defaultColor: '#000000',
+          defaultSize: 5
+        }, opts);
+        this.painting = false;
+        this.color = this.options.defaultColor;
+        this.size = this.options.defaultSize;
+        this.tool = this.options.defaultTool;
+        this.actions = [];
+        this.action = [];
+      }
+      Sketch.prototype.startPainting = function() {
+        this.painting = true;
+        return this.action = {
+          tool: this.tool,
+          color: this.color,
+          size: this.size,
+          events: []
+        };
+      };
+      Sketch.prototype.stopPainting = function() {
+        this.actions.push(this.action);
+        this.painting = false;
+        this.action = null;
+        return this.redraw();
+      };
+      Sketch.prototype.onEvent = function(e) {
+        return $(this).data('sketch').addEvent(e);
+      };
+      Sketch.prototype.addEvent = function(e) {
+        switch (e.type) {
+          case 'mousedown':
+            this.startPainting();
+            break;
+          case 'mouseup':
+          case 'mouseout':
+            this.stopPainting();
+        }
+        if (this.painting) {
+          this.action.events.push({
+            x: e.pageX - this.canvas.offset().left,
+            y: e.pageY - this.canvas.offset().top,
+            event: e.type,
+            color: this.color,
+            size: this.size,
+            tool: this.tool
+          });
+          return this.redraw();
+        }
+      };
+      Sketch.prototype.redraw = function() {
+        var sketch;
+        this.el.width = this.el.width;
+        sketch = this;
+        $.each(this.actions, function() {
+          return $.sketch.tools[this.tool].draw.call(sketch, this);
+        });
+        if (this.painting) {
+          return $.sketch.tools[this.action.tool].draw.call(sketch, this.action);
+        }
+      };
+      return Sketch;
+    })();
+    $.fn.sketch = function(opts) {
+      var that;
       if (this.length > 1) {
         $.error('Sketch can only be called on one element at a time.');
       }
-      options = $.extend({
-        toolLinks: true
-      }, opts);
-      el = this.get(0);
-      $el = $(el);
-      context = el.getContext('2d');
-      $el.data('sketch.clicks', []);
-      $el.data('sketch.painting', false);
-      $el.data('sketch.color', '#000000');
-      painting = function() {
-        return !!$el.data('sketch.painting');
-      };
-      startPainting = function() {
-        return $el.data('sketch.painting', true);
-      };
-      stopPainting = function() {
-        return $el.data('sketch.painting', false);
-      };
-      clicks = function() {
-        return $el.data('sketch.clicks');
-      };
-      currentColor = function() {
-        return $el.data('sketch.color');
-      };
-      addClick = function(x, y, dragging) {
-        return clicks().push({
-          x: x,
-          y: y,
-          dragging: dragging,
-          color: currentColor()
-        });
-      };
-      redraw = function() {
-        var previous;
-        el.width = el.width;
-        context.lineJoin = 'round';
-        context.lineWidth = 5;
-        previous = null;
-        return $.each(clicks(), function() {
-          context.beginPath();
-          if (this.dragging && previous) {
-            context.moveTo(previous.x, previous.y);
-          } else {
-            context.moveTo(this.x - 1, this.y);
-          }
-          context.lineTo(this.x, this.y);
-          context.closePath();
-          context.strokeStyle = this.color;
-          context.stroke();
-          return previous = this;
-        });
-      };
-      $el.bind('sketch.changecolor', function(e, color) {
-        console.log("Setting color to " + color);
+      this.data('sketch', new Sketch(this.get(0), opts));
+      this.bind('sketch.changecolor', function(e, color) {
         return $el.data('sketch.color', color);
       });
-      $el.mousedown(function(e) {
-        var mouseX, mouseY;
-        mouseX = e.pageX - this.offsetLeft;
-        mouseY = e.pageY - this.offsetTop;
-        startPainting();
-        addClick(mouseX, mouseY);
-        return redraw();
-      });
-      $el.mousemove(function(e) {
-        var mouseX, mouseY;
-        if (painting()) {
-          mouseX = e.pageX - this.offsetLeft;
-          mouseY = e.pageY - this.offsetTop;
-          addClick(mouseX, mouseY, true);
-          return redraw();
-        }
-      });
-      $el.mouseup(stopPainting);
-      $el.mouseleave(stopPainting);
-      if (options.toolLinks) {
-        console.log("Tool links...");
-        $('body').delegate("a[href=\"#" + ($el.attr('id')) + "\"]", 'click', function(e) {
+      this.mousedown(this.data('sketch').onEvent);
+      this.mousemove(this.data('sketch').onEvent);
+      this.mouseup(this.data('sketch').onEvent);
+      this.mouseleave(this.data('sketch').onEvent);
+      that = this;
+      if (this.data('sketch').options.toolLinks) {
+        $('body').delegate("a[href=\"#" + (that.attr('id')) + "\"]", 'click', function(e) {
           var $this;
-          console.log("Tool clicked...");
           $this = $(this);
           if ($this.attr('data-color')) {
-            $el.trigger('sketch.changecolor', $(this).attr('data-color'));
+            that.trigger('sketch.changecolor', $(this).attr('data-color'));
           }
           return false;
         });
       }
       return this;
+    };
+    return $.sketch.tools.marker = {
+      draw: function(action) {
+        var event, previous, _i, _len, _ref, _results;
+        this.context.lineJoin = "round";
+        previous = null;
+        _ref = action.events;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          event = _ref[_i];
+          this.context.beginPath();
+          if (previous) {
+            this.context.moveTo(previous.x, previous.y);
+          } else {
+            this.context.moveTo(event.x - 1, event.y);
+          }
+          this.context.lineTo(event.x, event.y);
+          this.context.closePath();
+          this.context.strokeStyle = action.color;
+          this.context.lineWidth = action.size;
+          this.context.stroke();
+          _results.push(previous = event);
+        }
+        return _results;
+      }
     };
   })(jQuery);
 }).call(this);
